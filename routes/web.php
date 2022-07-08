c1dafb77d9bd1bcc7030c4dc2e3046d9b591b36e
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2022-07-08 15:17:45 +0800

    creating listings links

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing as ModelsListing;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,22 +14,37 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//ALL Listings
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome',
+    [
+        'heading'=>'Latest Gigs',
+       'listings'=>ModelsListing::all(),
+      
+       ]
+);
 });
 
-Route::get("/hello",function(){
-    return response('<h1>Hello</h1>',200)
-    ->header('Content-Type','text/plain')
-    ->header('foo','bar');
-});
+// Route::get("/hello",function(){
+//     return response('<h1>Hello</h1>',200)
+//     ->header('Content-Type','text/plain')
+//     ->header('foo','bar');
+// });
 
-Route::get("/posts/{id}",function($id){
-    ddd($id);
-    return response('Post'.$id);
-})->where('id','[0-9]+');
+// Route::get("/posts/{id}",function($id){
+//     ddd($id);
+//     return response('Post'.$id);
+// })->where('id','[0-9]+');
 
-Route::get('/search',function(Request $request){
-    dd($request->name.' '.$request->city);
+// Route::get('/search',function(Request $request){
+//     dd($request->name.'   '.$request->city);
+// });
+
+Route::get('/listings/{id}',function($id){
+    return view('listing',
+    [
+       'listing'=>ModelsListing::find($id),
+      
+       ]
+);
 });
