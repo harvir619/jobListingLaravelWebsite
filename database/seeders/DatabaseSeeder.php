ffdc7c1e4081c25f9413614797ac5cc32c9dcc88
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2022-07-08 16:43:29 +0800

    database connected and theme setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Listing;
+use Database\Factories\ListingFactorFactory;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -42,6 +42,11 @@ class DatabaseSeeder extends Seeder
             'website' => 'https://www.wayneenterprises.com',
             'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
           ]);
+
+
+        Listing::factory(6)->create();
+
+       
 
 
 
