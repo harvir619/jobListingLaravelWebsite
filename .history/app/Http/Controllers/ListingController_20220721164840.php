33ad16fd347a8fa4c72e94133b6c66cb5ec5fabc
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2022-07-21 16:52:36 +0800

    Update,Delete,File Upload

---
+++ .history/app/Http/Controllers/ListingController_20220721164840.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class ListingController extends Controller
+{
+    //Get and Show All Listings
+    public function index()
+    {
+        //dd(request('tag'));
+        return view(
+            'listings.index',
+            [
+                'heading' => 'Latest Gigs',
+                'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(6),
+
+            ]
+        );
+    }
+    //Show Single Listing
+    public function show(Listing $listing)
+    {
+        return view('listings.show', ['listing' => $listing,]);
+    }
+
+
+    //Show Create Form
+    public function create()
+    {
+        return view('listings.create');
+    }
+
+    //Store Form Listing Data
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        Listing::create($formFields);
+
+
+        return redirect('/')->with('message', 'Listing created Successfully!');
+    }
+
+
+
+    //Show Edit Form
+    public function edit(Listing $listing)
+    {
+        return view('listings.edit', ['listing' => $listing]);
+    }
+
+    public function update(Request $request, Listing $listing)
+    {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => 'required',
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields);
+
+
+        return back()->with('message', 'Listing updated Successfully!');
+    }
+
+
+    public function destroy(Request $)
+}
---
+++ .history/routes/web_20220721154514.php
@@ -0,0 +1,65 @@
+<?php
+
+use App\Http\Controllers\ListingController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//ALL Listings
+Route::get('/', [ListingController::class,'index'] );
+
+
+//Show Create Form
+Route::get('/listings/create',[ListingController::class,'create']);
+
+//Store Listing Data
+Route::post('/listings',[ListingController::class,'store']);
+
+
+
+//Single Listing
+Route::get('/listings/{listing}',[ListingController::class,'show']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Route::get("/hello",function(){
+//     return response('<h1>Hello</h1>',200)
+//     ->header('Content-Type','text/plain')
+//     ->header('foo','bar');
+// });
+
+// Route::get("/posts/{id}",function($id){
+//     ddd($id);
+//     return response('Post'.$id);
+// })->where('id','[0-9]+');
+
+// Route::get('/search',function(Request $request){
+//     dd($request->name.'   '.$request->city);
+// });
+
+
+
+// Route::get('/listings/{id}',function($id){
+//     $listing =ModelsListing::find($id);
+//     if($listing){
+//         return view('listing',['listing'=>$listing,]);
+//     } else{ abort('404'); }});
